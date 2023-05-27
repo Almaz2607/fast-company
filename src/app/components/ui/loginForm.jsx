cb74b250666309f06e5f53172277f34205a5c3bd
Almaz Kubakaev <almazkubakaev2607@gmail.com>
2023-05-27 10:38:06 +0600

    theme 'form-part2' validate with yup

--- src/app/components/ui/loginForm.jsx
+++ src/app/components/ui/loginForm.jsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
-import { validator } from "../../utils/validator";
+// import { validator } from "../../utils/validator";
 import TextField from "../common/form/textField";
 import CheckBoxField from "../common/form/checkBoxField";
+import * as yup from "yup";
 
 const LoginForm = () => {
     const [data, setData] = useState({
@@ -10,49 +11,78 @@ const LoginForm = () => {
         stayOn: false
     });
     const [errors, setErrors] = useState({});
-
-    useEffect(() => {
-        validate();
-    }, [data]);
-
-    const validate = () => {
-        const errors = validator(data, validatorConfig);
-        setErrors(errors);
-        return Object.keys(errors).length === 0;
-    };
-
-    const isValid = Object.keys(errors).length === 0;
-
-    const validatorConfig = {
-        email: {
-            isRequired: {
-                message: "Электронная почта обязательна для заполнения"
-            },
-            isEmail: {
-                message: "Email введен некорректно"
-            }
-        },
-        password: {
-            isRequired: { message: "Пароль обязателен для заполнения" },
-            isCapitalSymbol: {
-                message: "Пароль должен содержать хотя бы одну заглавную букву"
-            },
-            isContainDigit: {
-                message: "Пароль должен содержать хотя бы одну цифру"
-            },
-            min: {
-                message: "Длина пароля должна состоять минимум из 8 символов",
-                value: 8
-            }
-        }
-    };
-
     const handleChange = (target) => {
         setData((prevState) => ({
             ...prevState,
             [target.name]: target.value
         }));
     };
+
+    const validateScheme = yup.object().shape({
+        password: yup
+            .string()
+            .required("Пароль обязателен для заполнения")
+            .matches(
+                /(?=.*[A-Z])/,
+                "Пароль должен содержать хотя бы одну заглавную букву"
+            )
+            .matches(
+                /(?=.*[0-9])/,
+                "Пароль должен содержать хотя бы одну цифру"
+            )
+            .matches(
+                /(?=.*[!@#$%^&*])/,
+                "Пароль должен содержать один из специальных символов !@#$%^&*"
+            )
+            .matches(
+                /(?=.{8,})/,
+                "Длина пароля должна состоять минимум из 8 символов"
+            ),
+        email: yup
+            .string()
+            .required("Электронная почта обязательна для заполнения")
+            .email("Email введен некорректно")
+    });
+
+    //  const validatorConfig = {
+    //      email: {
+    //          isRequired: {
+    //              message: "Электронная почта обязательна для заполнения"
+    //          },
+    //          isEmail: {
+    //              message: "Email введен некорректно"
+    //          }
+    //      },
+    //      password: {
+    //          isRequired: { message: "Пароль обязателен для заполнения" },
+    //          isCapitalSymbol: {
+    //              message: "Пароль должен содержать хотя бы одну заглавную букву"
+    //          },
+    //          isContainDigit: {
+    //              message: "Пароль должен содержать хотя бы одну цифру"
+    //          },
+    //          min: {
+    //              message: "Длина пароля должна состоять минимум из 8 символов",
+    //              value: 8
+    //          }
+    //      }
+    //  };
+
+    useEffect(() => {
+        validate();
+    }, [data]);
+
+    const validate = () => {
+        //   const errors = validator(data, validatorConfig);
+        validateScheme
+            .validate(data)
+            .then(() => setErrors({}))
+            .catch((err) => setErrors({ [err.path]: err.message }));
+        //   setErrors(errors);
+        return Object.keys(errors).length === 0;
+    };
+
+    const isValid = Object.keys(errors).length === 0;
 
     const handleSubmit = (e) => {
         e.preventDefault();
